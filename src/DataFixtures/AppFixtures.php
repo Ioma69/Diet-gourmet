bb723d983448a27bad343cc967ebc9099e9dc460
Sariel RAQUIN <sarielr@hotmail.fr>
2023-05-30 17:40:30 +0200

    add fixtures + function create patient for the admin

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher) {
+        $this->passwordHasher = $passwordHasher;
+        
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+         $Sandrine = new Admin($this->passwordHasher);
+         $Sandrine->setEmail("SandrineCoupart@hotmail.com")->setPassword("SandrineCoupart")->setRoles(["ROLE_ADMIN"]);
+         $manager->persist($Sandrine);
+         $manager->flush();
+
+    }
+
+    
+}
