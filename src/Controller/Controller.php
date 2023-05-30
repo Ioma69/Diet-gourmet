03921b11c69700dd5359a7da382bc2f305385468
Sariel RAQUIN <sarielr@hotmail.fr>
2023-05-30 13:47:41 +0200

    create entities admin and user + form login and form user

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
+use PhpParser\Node\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Controller extends AbstractController
 {
-    #[Route('/', name: 'app_')]
+    #[Route('/', name:"home")]
     public function index(): Response
     {
-        return $this->render('/index.html.twig', [
-            'controller_name' => 'Controller',
-        ]);
+        return $this->render('/index.html.twig');
     }
 }
